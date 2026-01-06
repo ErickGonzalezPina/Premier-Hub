5e21490123e62f52bf33e365dc2c51380b241500
ErickGonzalezPina <erickbgonzalezp10@gmail.com>
2026-01-06 12:29:40 -0500

    first commit, updated the properties file to connect to the database

---
+++ src/test/java/com/erick/premier_hub/PremierHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erick.premier_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

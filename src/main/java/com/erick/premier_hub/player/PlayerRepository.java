e44fbb572f39829bdbea45b2b59b5230c5312527
ErickGonzalezPina <erickbgonzalezp10@gmail.com>
2026-01-06 12:46:02 -0500

    Add JPA repository layer

---
+++ src/main/java/com/erick/premier_hub/player/PlayerRepository.java
@@ -0,0 +1,16 @@
+package com.erick.premier_hub.player;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, String> {
+    // these are optional since the Repository layer has them by default
+    void deleteByName(String playerName);
+
+    Optional<Player> findByName(String name);
+}

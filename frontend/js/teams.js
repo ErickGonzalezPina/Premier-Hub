1556252de45761ec55aa98cf767beed568d2ca25
ErickGonzalezPina <erickbgonzalezp10@gmail.com>
2026-01-06 19:55:48 -0500

    Create teams overview page with team logo grid

---
+++ frontend/js/teams.js
@@ -0,0 +1,45 @@
+
+
+
+function loadTeams() {
+    var teamsContainer = document.getElementById('teams_container');
+    teamsContainer.innerHTML = '';
+
+    const teamNames = [
+        "Bournemouth",
+        "Everton",
+        "Crystal-Palace",
+        "Manchester-City",
+        "Nottingham-Forest",
+        "Wolverhampton-Wanderers",
+        "Manchester-United",
+        "Fulham",
+        "Brentford",
+        "Aston-Villa",
+        "Liverpool",
+        "Newcastle-United",
+        "Leicester-City",
+        "Arsenal",
+        "Leeds-United",
+        "Brighton-and-Hove-Albion",
+        "Southampton",
+        "West-Ham-United",
+        "Tottenham-Hotspur",
+        "Chelsea"
+    ];
+
+    teamNames.forEach(function(team) {
+        var teamDiv = document.createElement("div");
+        teamDiv.className = "team_card";
+
+        var teamLogo = document.createElement("img");
+        teamLogo.src = "assets/teams/" + team + ".png";
+        teamLogo.alt = team + " Logo";
+        teamLogo.className = "team_logo_img";
+        
+        teamDiv.appendChild(teamLogo);
+        teamsContainer.appendChild(teamDiv);
+    });
+}
+
+loadTeams();
